7765e9b90305f264fff12cd5ee8c65f7be2a07ae
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-10-05 15:26:43 -0300

    refactor struct

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Vasak',
-    packages=find_packages(include=['Vasak']),
+    packages=find_packages(include=['Vasak', 'Vasak.*']),
     version='0.1.1',
     description='Vasak is a Python library for creating desktop applications with web technologies.',
     author='Vasak Group',
