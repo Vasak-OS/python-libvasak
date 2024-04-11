35feae4b1cf4c1e8c0bd85d31ad596cfb255b6c6
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2024-04-11 17:08:28 -0300

    fix: Actualizar la generación de datos JSON en VSKDisks.py

--- Vasak/hardware/VSKDisks.py
+++ Vasak/hardware/VSKDisks.py
@@ -6,22 +6,19 @@ class VSKDisks:
     self.diskInfo = DiskInfo.DiskInfo()
 
   def getDisksList(self):
-    jsondata = "["
+    jsondata = []
     for disk in self.diskInfo.get_disk_list():
-      print(disk)
-      jsondata += self.diskToStringJSON(disk) + ","
-    jsondata += "]"
-    print(jsondata)
-    return jsondata
+      jsondata.append(self.diskToJSON(disk))
+    return json.dumps(jsondata)
   
-  def diskToStringJSON(self, disk):
+  def diskToJSON(self, disk):
     datadisk = {
       "name": disk.get_name(),
       "deviceId": disk.get_device_id(),
       "model": disk.get_model(),
       "partition": {
         "type": disk.get_partition_table_type(),
-        "list": self.partitionListToStringJSON(disk.get_partition_list()),
+        "list": self.partitionListToJSON(disk.get_partition_list()),
         "uuid": disk.get_partition_table_uuid()
       },
       "path": disk.get_path(),
@@ -29,17 +26,16 @@ class VSKDisks:
       "size": disk.get_size() * 512,
       "type": disk.get_type_str(),
     }
-    return str(datadisk)
+    return datadisk
   
-  def partitionListToStringJSON(self, partitionList):
-    jsondata = "["
+  def partitionListToJSON(self, partitionList):
+    partitions = []
     for partition in partitionList:
-      jsondata += self.partitionToStringJSON(partition) + ","
-    jsondata += "]"
-    return jsondata
+      partitions.append(self.partitionToJSON(partition)) 
+    return partitions
   
-  def partitionToStringJSON(self, partition):
-    if partition.get_part_scheme() == "GPT":
+  def partitionToJSON(self, partition):
+    if partition.get_part_scheme() == "gpt":
       datapartition = {
         "path": partition.get_path(),
         "name": partition.get_name(),
@@ -75,5 +71,4 @@ class VSKDisks:
         "fsLabel": partition.get_fs_label(),
         "fsFreeSize": partition.get_fs_free_size() * 512,
       }
-    
-    return str(datapartition)
+    return datapartition
